80c83b534c8525c490e51398fc077ea9d90802bf
Pablo Gómez <pablogomez@pablogomez.com>
2025-08-16 22:26:19 +0100

    Update package.nix

--- pkgs/by-name/op/opencode/package.nix
+++ pkgs/by-name/op/opencode/package.nix
@@ -28,7 +28,7 @@ let
 in
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "opencode";
-  version = "0.4.26";
+  version = "0.5.5";
   src = fetchFromGitHub {
     owner = "sst";
     repo = "opencode";
